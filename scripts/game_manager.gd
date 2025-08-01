f85aeabf0cb04c5d9396a8336d15f0869f99a6a0
Brandon Dodd <brandonharleydodd@gmail.com>
2025-08-01 19:00:18 +0100

    Fix vignette bug

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,17 +1,18 @@
 extends Node
 
+const DeathVignette = preload("res://scripts/ui/death_vignette.gd")
 const TimerLabel = preload("res://scripts/ui/timer_label.gd")
 
-var vignette_material: ShaderMaterial
+var death_vignette: DeathVignette
 var timer_label: TimerLabel
-var vignette_tween: Tween
-var deaths := 0
+var deaths: int = 1
 
 func _ready():
-	vignette_material = get_node("../Main/GameUI/DeathVignette").material as ShaderMaterial
+	death_vignette = get_node("../Main/GameUI/DeathVignette") as DeathVignette
 	timer_label = get_node("../Main/GameUI/TimerLabel") as TimerLabel
-	timer_label.hide()
 
+	timer_label.hide()
+	death_vignette.set_death_completed_callback(_reset_loop)
 	start_countdown_dialogue()
 
 func start_countdown_dialogue():
@@ -24,76 +25,19 @@ func start_countdown_dialogue():
 	layout.register_character(load("res://dialogue/characters/Sophie.dch"), get_node("../Main/Characters/Sophie"))
 	layout.register_character(load("res://dialogue/characters/Thomas.dch"), get_node("../Main/Characters/Thomas"))
 
+func pulse_vignette():
+	death_vignette.play_pulse_animation()
+
 func trigger_death():
-	fade_in_vignette()
-	vignette_tween.finished.connect(func():
-		deaths += 1
-		get_tree().create_timer(1).timeout.connect(func():
-			reset_vignette(true)
-			vignette_tween.finished.connect(func():
-				start_countdown_dialogue()
-				if deaths >= 3: start_timer()
-			)
-		)
-	)
+	deaths += 1
+	death_vignette.play_death_animation()
+
+func _reset_loop():
+	start_countdown_dialogue()
+	if deaths >= 3: start_timer()
 
 func show_timer():
 	timer_label.show()
 
 func start_timer():
 	timer_label.start()
-
-func pulse_vignette(duration := 2, max_alpha := 1.0, end_alpha := 0.5):
-	vignette_tween = create_tween().set_trans(Tween.TRANS_SINE)
-	vignette_tween.tween_method(
-		func(val): vignette_material.set_shader_parameter("alpha", val),
-		vignette_material.get_shader_parameter("alpha"), max_alpha, duration * 0.5
-	)
-	vignette_tween.tween_method(
-		func(val): vignette_material.set_shader_parameter("alpha", val),
-		max_alpha, end_alpha, duration * 0.5
-	)
-
-func reset_vignette(tween := false):
-	if tween:
-		vignette_tween = create_tween().set_trans(Tween.TRANS_SINE).set_parallel(true)
-		vignette_tween.tween_method(
-			func(val): vignette_material.set_shader_parameter("alpha", val),
-			vignette_material.get_shader_parameter("alpha"), 0.0, 1.0
-		)
-		vignette_tween.tween_method(
-			func(val): vignette_material.set_shader_parameter("inner_radius", val),
-			vignette_material.get_shader_parameter("inner_radius"), 0.55, 1.0
-		)
-		vignette_tween.tween_method(
-			func(val): vignette_material.set_shader_parameter("outer_radius", val),
-			vignette_material.get_shader_parameter("outer_radius"), 1.235, 1.0
-		)
-		vignette_tween.tween_method(
-			func(val): vignette_material.set_shader_parameter("color", val),
-			vignette_material.get_shader_parameter("color"), Color(72, 0, 0), 1.0
-		)
-	else:
-		vignette_material.set_shader_parameter("alpha", 0.0)
-		vignette_material.set_shader_parameter("inner_radius", 0.55)
-		vignette_material.set_shader_parameter("outer_radius", 1.235)
-		vignette_material.set_shader_parameter("color", Color(72, 0, 0))
-
-func fade_in_vignette(target_alpha := 1, duration := 3, target_inner_radius := 0, target_outer_radius := 0, target_color := Color(0, 0, 0), color_delay := 2.5):
-	vignette_tween = create_tween().set_trans(Tween.TRANS_SINE).set_parallel(true)
-	vignette_tween.tween_method(
-		func(val): vignette_material.set_shader_parameter("alpha", val),
-		vignette_material.get_shader_parameter("alpha"), target_alpha, duration
-	)
-	vignette_tween.tween_method(
-		func(val): vignette_material.set_shader_parameter("inner_radius", val),
-		vignette_material.get_shader_parameter("inner_radius"), target_inner_radius, duration
-	)
-	vignette_tween.tween_method(
-		func(val): vignette_material.set_shader_parameter("outer_radius", val),
-		vignette_material.get_shader_parameter("outer_radius"), target_outer_radius, duration * 2
-	)
-	vignette_tween.tween_method(
-		func(val): vignette_material.set_shader_parameter("color", val),
-		vignette_material.get_shader_parameter("color"), target_color, duration
-	).set_delay(color_delay)
---
+++ scripts/ui/death_vignette.gd
@@ -0,0 +1,95 @@
+extends ColorRect
+
+enum DeathState { NONE, FADING_IN, WAITING, FADING_OUT, RESTARTING }
+var death_state = DeathState.NONE
+
+var tween: Tween
+var on_death_completed: Callable
+
+func play_death_animation():
+	if death_state != DeathState.NONE: return
+	death_state = DeathState.FADING_IN
+	fade_in_vignette()
+
+	if tween.finished.is_connected(_on_fade_in_finished):
+		tween.finished.disconnect(_on_fade_in_finished)
+
+	tween.finished.connect(_on_fade_in_finished, CONNECT_ONE_SHOT)
+
+func _on_fade_in_finished():
+	death_state = DeathState.WAITING
+	get_tree().create_timer(1.0).timeout.connect(_on_death_wait_timeout, CONNECT_ONE_SHOT)
+	
+func _on_death_wait_timeout():
+	death_state = DeathState.FADING_OUT
+	fade_out_vignette()
+	
+	if tween.finished.is_connected(_on_fade_out_finished):
+		tween.finished.disconnect(_on_fade_out_finished)
+
+	tween.finished.connect(_on_fade_out_finished, CONNECT_ONE_SHOT)
+	
+func _on_fade_out_finished():
+	death_state = DeathState.RESTARTING
+	if on_death_completed: on_death_completed.call()
+	death_state = DeathState.NONE
+
+func set_death_completed_callback(callback: Callable):
+	on_death_completed = callback
+
+func play_pulse_animation(duration := 2.0):
+	if death_state != DeathState.NONE: return
+		
+	if tween: tween.kill()
+	tween = create_tween().set_trans(Tween.TRANS_SINE)
+
+	tween.tween_method(
+		func(val): material.set_shader_parameter("alpha", val),
+		material.get_shader_parameter("alpha"), 1.0, duration * 0.5
+	)
+	tween.tween_method(
+		func(val): material.set_shader_parameter("alpha", val),
+		1.0, 0.5, duration * 0.5
+	)
+
+func fade_out_vignette():
+	if tween: tween.kill()
+	tween = create_tween().set_trans(Tween.TRANS_SINE).set_parallel(true)
+
+	tween.tween_method(
+		func(val): material.set_shader_parameter("alpha", val),
+		material.get_shader_parameter("alpha"), 0.0, 1.0
+	)
+	tween.tween_method(
+		func(val): material.set_shader_parameter("inner_radius", val),
+		material.get_shader_parameter("inner_radius"), 0.55, 1.0
+	)
+	tween.tween_method(
+		func(val): material.set_shader_parameter("outer_radius", val),
+		material.get_shader_parameter("outer_radius"), 1.235, 1.0
+	)
+	tween.tween_method(
+		func(val): material.set_shader_parameter("color", val),
+		material.get_shader_parameter("color"), Color(0.282, 0, 0), 1.0
+	)
+
+func fade_in_vignette():
+	if tween: tween.kill()
+	tween = create_tween().set_trans(Tween.TRANS_SINE).set_parallel(true)
+	
+	tween.tween_method(
+		func(val): material.set_shader_parameter("alpha", val),
+		material.get_shader_parameter("alpha"), 1.0, 3.0
+	)
+	tween.tween_method(
+		func(val): material.set_shader_parameter("inner_radius", val),
+		material.get_shader_parameter("inner_radius"), 0.0, 3.0
+	)
+	tween.tween_method(
+		func(val): material.set_shader_parameter("outer_radius", val),
+		material.get_shader_parameter("outer_radius"), 0.0, 6.0
+	)
+	tween.tween_method(
+		func(val): material.set_shader_parameter("color", val),
+		material.get_shader_parameter("color"), Color(0, 0, 0), 3.0
+	).set_delay(2.5)
